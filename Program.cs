2192435ea72492eb335a1e5ea4518a0a1c66c4d0
nakyumdepzai <nguyenvietnamkhanhbl@gmail.com>
2024-03-29 02:17:46 +0700

    Update Error View

--- Program.cs
+++ Program.cs
@@ -103,7 +103,8 @@ namespace TimeShareProject
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            app.UseStatusCodePagesWithRedirects("/Error/ErrorNotFound");
+                
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
